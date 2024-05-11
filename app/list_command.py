c8ce17d22895fc09e69328e60502d266856f227b
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-11 17:02:17 -0300

    added Github and docker integration, async execution and multi org support

--- app/list_command.py
+++ app/list_command.py
@@ -1,7 +1,17 @@
+import asyncio
+import json
 import math
+import os
+import re
 from pathlib import Path
 
+import docker
+import github
 import texttable
+from github import Auth
+from github import Github
+
+INSTALLATION_FOLDER = Path(Path.home().joinpath('procli'))
 
 
 class Style:
@@ -37,68 +47,88 @@ class Style:
 
 
 class Icons:
+    # unicode characters: https://www.tamasoft.co.jp/en/general-info/unicode.html
     not_installed = f'{Style.white}☐{Style.reset}'
     not_installed2 = f'{Style.white}⊘{Style.reset}'
+    not_installed3 = f'{Style.white}ﰮ{Style.reset}'
+
     running = f'{Style.green}☑{Style.reset}'
     running2 = f'{Style.green}✔{Style.reset}'
+    running3 = f'{Style.green}ﲏ{Style.reset}'
+
     stopped = f'{Style.red}☒{Style.reset}'
     stopped2 = f'{Style.red}✖{Style.reset}'
+    stopped3 = f'{Style.red}ﱥ{Style.reset}'
 
 
-def list_apps_command():
-    apps = get_apps()
-    formatted_matrix, max_width, num_cols = format_matrix(apps)
+async def list_apps_command():
+    apps_by_organization_on_github = dict()
+    # Dict struct example:
+    # organizations = {
+    #     org_name: {
+    #         app_name: {
+    #             instructions: dict
+    #         }
+    #     }
+    # }
+    apps_by_organization_locally = dict()
+    # Dict struct example:
+    # organizations = {
+    #     org_name: {
+    #         app_name: {
+    #             installed: bool,
+    #             running: bool,
+    #             instructions: dict
+    #         }
+    #     }
+    # }
+    async with asyncio.TaskGroup() as tg:
+        tg.create_task(get_organization_apps_on_github(apps_by_organization_on_github))
+        tg.create_task(get_organization_apps_locally(INSTALLATION_FOLDER, apps_by_organization_locally))
 
-    table = texttable.Texttable()
-    table.set_chars(['', '', '', ''])
-    table.set_cols_width([max_width] * num_cols)
-    table.add_rows(formatted_matrix, header=False)
-
-    header_name = 'Apps'
-    header_division_char = '='
-    header_division = math.floor((max_width + num_cols/2) * 4)
-    header_position = math.floor(header_division/2 - len(header_name)/2)
-    print(f'{header_position * " "}{header_name}')
-    print(header_division * header_division_char)
-    print(table.draw())
-
-
-def get_apps() -> list:
-    apps_path_var = ''
-    manifests_name = 'manifests'
-
-    apps_path = Path(Path.home().joinpath(apps_path_var))
-    if not apps_path.exists():
-        raise Exception("Installation path doesn't exists")
-
-    with open(apps_path.joinpath(manifests_name)) as file:
-        apps = [f"{line.split('.')[0]}" for line in file]
-
-    apps = [
-        {
-            'name': app_name,
-            'installed': get_app_install_status(app_name),
-            'running': get_app_running_status(app_name)
-        }
-        for app_name in apps
-    ]
-    return apps
+    merged_apps_by_organization = await merge_apps(apps_by_organization_locally, apps_by_organization_on_github)
+    show_apps_by_organization(merged_apps_by_organization)
 
 
-def get_app_install_status(app: str) -> bool:
-    pass
+async def merge_apps(apps_by_organization_locally, apps_by_organization_on_github):
+    # TODO: parallelize process
+    merged = apps_by_organization_on_github.copy()
+    for org in merged:
+        github_org_apps = apps_by_organization_on_github.get(org)
+        local_org_apps = apps_by_organization_locally.get(org)
+        if local_org_apps:
+            merged[org] = {**github_org_apps, **local_org_apps}
+
+    return merged
 
 
-def get_app_running_status(app: str) -> bool:
-    pass
+def show_apps_by_organization(merged_apps_by_organization: dict):
+    if not merged_apps_by_organization:
+        print('No apps found.')
+        return
+
+    for org in merged_apps_by_organization:
+        formatted_matrix, max_width, num_cols = format_matrix(merged_apps_by_organization[org])
+        table = texttable.Texttable()
+        table.set_chars(['', '', '', ''])
+        table.set_cols_width([max_width] * num_cols)
+        table.add_rows(formatted_matrix, header=False)
+        header_name = f'{org}'.capitalize()
+        header_division_char = '='
+        header_division = math.floor((max_width + num_cols / 2) * num_cols)
+        header_position = math.floor(header_division / 2 - len(header_name) / 2)
+        print(f'{" " * header_position}{header_name}')
+        print(header_division * header_division_char)
+        print(table.draw())
 
 
-def format_matrix(apps: list) -> (list, int, int):
-    max_num_cols = 4
-    apps.sort(key=lambda app: app['name'])
+def format_matrix(apps: dict) -> (list, int, int):
+    apps = [app.capitalize() for app in apps.keys()]
+    max_num_cols = 6
+    apps.sort()
     app_len = len(apps)
     num_cols = app_len if app_len <= max_num_cols else max_num_cols
-    num_rows = math.ceil(app_len/num_cols)
+    num_rows = math.ceil(app_len / num_cols)
     formatted_apps = [['' for _ in range(num_cols)] for _ in range(num_rows)]
     max_col_width = 0
     for i in range(app_len):
@@ -110,3 +140,103 @@ def format_matrix(apps: list) -> (list, int, int):
         formatted_apps[line][column] = apps[i]
 
     return formatted_apps, max_col_width, num_cols
+
+
+async def get_organization_apps_locally(path: Path, organizations: dict):
+    # TODO: parallelize process
+    create_installation_folder(path)
+    orgs = [org for org in path.iterdir() if org.is_dir()]
+    for org in orgs:
+        for app in path.joinpath(org).iterdir():
+            if app.is_dir():
+                # there are no duplicated orgs nor apps
+                org_name = org.name.lower()
+                app_name = app.name.lower()
+                organizations[org_name] = {
+                    app_name: {
+                        'installed': True,
+                        'running': get_app_running_status(app_name),
+                        'instructions': get_app_instructions(path.joinpath(org).joinpath(app)),
+                    }
+                }
+
+
+def create_installation_folder(path: Path):
+    if path.exists():
+        return
+
+    path.mkdir()
+
+
+async def get_organization_apps_on_github(organizations: dict):
+    pattern = re.compile(r'^GITHUB_(\w+)_TOKEN')
+    env_keys = os.environ.keys()
+    async with asyncio.TaskGroup() as tg:
+        for key in env_keys:
+            tg.create_task(get_organization_names(organizations, pattern, key))
+
+    if not organizations:
+        print('No tokens found. Please confirm env var is in the following format: GITHUB_COMPANY_TOKEN')
+
+    async with asyncio.TaskGroup() as tg:
+        for org in organizations:
+            tg.create_task(get_organization_repos(org, organizations[org]))
+
+
+async def get_organization_names(orgs: dict, match_pattern: re.Pattern, env: str):
+    match = re.fullmatch(match_pattern, env)
+    if match and match.groups():
+        org_name = match.groups()[0].lower()
+        orgs[org_name] = dict()
+
+
+async def get_organization_repos(org_name: str, org_apps: dict):
+    try:
+        token = os.environ.get(f'GITHUB_{org_name.upper()}_TOKEN')
+        gh = Github(auth=Auth.Token(token))
+        full_org_repos = gh.get_organization(org_name).get_repos()
+        async with asyncio.TaskGroup() as tg:
+            for repo in full_org_repos:
+                tg.create_task(get_repos_with_instructions(org_apps, repo))
+
+    except Exception as exc:
+        # TODO: Handle error
+        pass
+
+
+async def get_repos_with_instructions(repos: dict, repo: github.Repository):
+    try:
+        instructions = json.loads(repo.get_contents('.procli.json').decoded_content.decode())
+        app_name = repo.name.lower()
+        repos[app_name] = {
+            'installed': False,
+            'running': False,
+            'instructions': instructions
+        }
+
+    except Exception as exc:
+        # TODO: Handle error
+        pass
+
+
+def get_app_running_status(app_name: str) -> bool:
+    try:
+        client = docker.from_env()
+        container = client.containers.get(app_name)
+        if container.status == 'running':
+            return True
+
+        return False
+    except Exception as exc:
+        # App not found on docker
+        return False
+
+
+def get_app_instructions(path: Path) -> str:
+    try:
+        with open(path.joinpath('.procli.json').resolve()) as f:
+            return json.loads(f.read())
+
+    except Exception as exc:
+        # TODO: Handle error
+        pass
