98cc16f1c29e07ad98925c4f90e7fb56c62aad5b
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-12 17:48:03 -0300

    added network check

---
+++ app/utils/network.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def is_network_ok() -> bool:
+    url = "http://www.google.com"
+    timeout = 1
+    try:
+        response = requests.get(url, timeout=timeout)
+        if response.status_code == 200:
+            return True
+
+        return False
+    except Exception as exc:
+        print(exc)
+        return False
