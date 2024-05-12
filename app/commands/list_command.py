8a4bc6317663fa2f7bb3c875f1c0a852607e2817
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-12 18:36:50 -0300

    parallelized process

--- app/commands/list_command.py
+++ app/commands/list_command.py
@@ -6,53 +6,6 @@ from app.utils.printer import show_apps_by_organization
 from app.utils.repo import get_organization_apps_on_github
 
 
-class Style:
-    csi = '\x1b['
-    reset = '\x1b[0m'
-    bold = '\x1b[1m'
-    italic = '\x1b[3m'
-    underline = '\x1b[4m'
-    fontDefault = '\x1b[10m'
-    font2 = '\x1b[11m'
-    font3 = '\x1b[12m'
-    font4 = '\x1b[13m'
-    font5 = '\x1b[14m'
-    font6 = '\x1b[15m'
-    imageNegative = '\x1b[7m'
-    imagePositive = '\x1b[27m'
-    black = '\x1b[30m'
-    red = '\x1b[31m'
-    green = '\x1b[32m'
-    yellow = '\x1b[33m'
-    blue = '\x1b[34m'
-    magenta = '\x1b[35m'
-    cyan = '\x1b[36m'
-    white = '\x1b[37m'
-    bg_black = '\x1b[40m'
-    bg_red = '\x1b[41m'
-    bg_green = '\x1b[42m'
-    bg_yellow = '\x1b[43m'
-    bg_blue = '\x1b[44m'
-    bg_magenta = '\x1b[45m'
-    bg_cyan = '\x1b[46m'
-    bg_white = '\x1b[47m'
-
-
-class Icons:
-    # unicode characters: https://www.tamasoft.co.jp/en/general-info/unicode.html
-    not_installed = f'{Style.white}☐{Style.reset}'
-    not_installed2 = f'{Style.white}⊘{Style.reset}'
-    not_installed3 = f'{Style.white}ﰮ{Style.reset}'
-
-    running = f'{Style.green}☑{Style.reset}'
-    running2 = f'{Style.green}✔{Style.reset}'
-    running3 = f'{Style.green}ﲏ{Style.reset}'
-
-    stopped = f'{Style.red}☒{Style.reset}'
-    stopped2 = f'{Style.red}✖{Style.reset}'
-    stopped3 = f'{Style.red}ﱥ{Style.reset}'
-
-
 async def list_apps_command():
     apps_by_organization_on_github = dict()
     # Dict struct example:
@@ -79,7 +32,7 @@ async def list_apps_command():
         tg.create_task(get_organization_apps_locally(apps_by_organization_locally))
 
     merged_apps_by_organization = await merge_apps(apps_by_organization_locally, apps_by_organization_on_github)
-    show_apps_by_organization(merged_apps_by_organization)
+    await show_apps_by_organization(merged_apps_by_organization)
 
 
 async def merge_apps(apps_by_organization_locally, apps_by_organization_on_github):
--- app/utils/local.py
+++ app/utils/local.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 from pathlib import Path
 
@@ -8,32 +9,41 @@ INSTALLATION_FOLDER = Path(Path.home().joinpath('procli'))
 
 
 async def get_organization_apps_locally(organizations: dict, path=INSTALLATION_FOLDER):
-    # TODO: parallelize process
-    create_installation_folder(path)
+    await create_installation_folder(path)
     orgs = [org for org in path.iterdir() if org.is_dir()]
-    for org in orgs:
+    async with asyncio.TaskGroup() as tg:
+        for org in orgs:
+            tg.create_task(get_apps_from_org(org, organizations, path))
+
+
+async def get_apps_from_org(org, organizations, path):
+    async with asyncio.TaskGroup() as tg:
         for app in path.joinpath(org).iterdir():
-            if app.is_dir():
-                # there are no duplicated orgs nor apps
-                org_name = org.name.lower()
-                app_name = app.name.lower()
-                organizations[org_name] = {
-                    app_name: {
-                        'installed': True,
-                        'running': get_app_running_status(app_name),
-                        'instructions': get_app_instructions(path.joinpath(org).joinpath(app)),
-                    }
-                }
+            tg.create_task(get_app_infos(app, org, organizations, path))
 
 
-def create_installation_folder(path: Path):
+async def get_app_infos(app, org, organizations, path):
+    if app.is_dir():
+        # there are no duplicated orgs nor apps
+        org_name = org.name.lower()
+        app_name = app.name.lower()
+        organizations[org_name] = {
+            app_name: {
+                'installed': True,
+                'running': await get_app_running_status(app_name),
+                'instructions': await get_app_instructions(path.joinpath(org).joinpath(app)),
+            }
+        }
+
+
+async def create_installation_folder(path: Path):
     if path.exists():
         return
 
     path.mkdir()
 
 
-def get_app_running_status(app_name: str) -> bool:
+async def get_app_running_status(app_name: str) -> bool:
     try:
         client = docker.from_env()
         container = client.containers.get(app_name)
@@ -46,7 +56,7 @@ def get_app_running_status(app_name: str) -> bool:
         return False
 
 
-def get_app_instructions(path: Path) -> str:
+async def get_app_instructions(path: Path) -> str:
     try:
         with open(path.joinpath('.procli.json').resolve()) as f:
             return json.loads(f.read())
