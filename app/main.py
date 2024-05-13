4037fb19e2157a8beacbb0075921d8f5c94aaa96
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-13 19:07:46 -0300

    refactored to use group

--- app/main.py
+++ app/main.py
@@ -2,22 +2,27 @@ import asyncio
 
 import click
 
-from app.commands.list_command import list_apps_command
+from app.commands.list import list_apps_command
+from utils import global_options
 
 
-@click.command(context_settings={'help_option_names': ['-h', '--help']})
-@click.option('-l', '--list', 'list_command', is_flag=True, default=False, help='List apps')
-def main(
-        list_command: bool,
+@click.group(context_settings={'help_option_names': ['-h', '--help']})
+def cli():
+    pass
+
+
+@cli.command(help='List apps.')
+@click.option('-a', '--show-all', is_flag=True, default=False, help='Show apps without instructions.')
+def list_apps(
+        show_all: bool
 ):
+    global_options.LIST_APPS_WITHOUT_INSTRUCTIONS = show_all
     try:
-        if list_command:
-            asyncio.run(list_apps_command())
-
+        asyncio.run(list_apps_command())
     except Exception as exc:
-        print(f'Exception: {exc}')
+        print(f'Error: {exc}')
         exit(1)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
