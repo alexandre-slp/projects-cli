3c1415b28a398bb5851efbb479e76f9ca285fd2d
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-13 11:04:53 -0300

    added network check

--- app/commands/list_command.py
+++ app/commands/list_command.py
@@ -2,6 +2,7 @@ import asyncio
 
 from app.utils.local import get_organization_apps_locally
 from app.utils.printer import show_apps_by_organization
+from app.utils.network import is_network_ok
 
 from app.utils.repo import get_organization_apps_on_github
 
@@ -28,23 +29,25 @@ async def list_apps_command():
     #     }
     # }
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(get_organization_apps_on_github(apps_by_organization_on_github))
+        if await is_network_ok():
+            tg.create_task(get_organization_apps_on_github(apps_by_organization_on_github))
+
         tg.create_task(get_organization_apps_locally(apps_by_organization_locally))
 
-    merged_apps_by_organization = await merge_apps(apps_by_organization_locally, apps_by_organization_on_github)
+    merged_apps_by_organization = await merge_github_with_local_apps(apps_by_organization_locally, apps_by_organization_on_github)
     await show_apps_by_organization(merged_apps_by_organization)
 
 
-async def merge_apps(apps_by_organization_locally, apps_by_organization_on_github):
+async def merge_github_with_local_apps(apps_by_organization_locally, apps_by_organization_on_github):
     merged = apps_by_organization_on_github.copy()
     async with asyncio.TaskGroup() as tg:
         for org in merged:
-            tg.create_task(method_name(apps_by_organization_locally, apps_by_organization_on_github, merged, org))
+            tg.create_task(merge_org_apps(apps_by_organization_locally, apps_by_organization_on_github, merged, org))
 
     return merged
 
 
-async def method_name(apps_by_organization_locally: dict, apps_by_organization_on_github: dict, merged: dict, org: str):
+async def merge_org_apps(apps_by_organization_locally: dict, apps_by_organization_on_github: dict, merged: dict, org: str):
     github_org_apps = apps_by_organization_on_github.get(org)
     local_org_apps = apps_by_organization_locally.get(org)
     if local_org_apps:
