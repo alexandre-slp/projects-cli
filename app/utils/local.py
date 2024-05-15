28575c900b784949b289d5240ad826e8e4fa07ad
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-15 11:51:28 -0300

    refactored adding hints and creating get_organizations

--- app/utils/local.py
+++ app/utils/local.py
@@ -11,29 +11,34 @@ INSTALLATION_FOLDER = Path(Path.home().joinpath('procli').resolve())
 
 
 async def get_organization_apps_locally(organizations: dict, path=INSTALLATION_FOLDER):
-    await create_installation_folder(path)
-    orgs = [org for org in path.iterdir() if org.is_dir()]
+    orgs = await get_organizations(path)
     async with asyncio.TaskGroup() as tg:
         for org in orgs:
             tg.create_task(get_apps_from_org(org, organizations, path))
 
 
-async def get_apps_from_org(org, organizations, path):
+async def get_organizations(path: Path) -> list[Path]:
+    await create_installation_folder(path)
+    orgs = [org for org in path.iterdir() if org.is_dir()]
+    return orgs
+
+
+async def get_apps_from_org(org_path: Path, organizations: dict, path: Path):
     async with asyncio.TaskGroup() as tg:
-        for app in path.joinpath(org).resolve().iterdir():
-            tg.create_task(get_app_infos(app, org, organizations, path))
+        for app in path.joinpath(org_path).resolve().iterdir():
+            tg.create_task(get_app_infos(app, org_path, organizations, path))
 
 
-async def get_app_infos(app, org, organizations, path):
-    if app.is_dir():
+async def get_app_infos(app_path: Path, org_path: Path, organizations: dict, path: Path):
+    if app_path.is_dir():
         # there are no duplicated orgs nor apps
-        org_name = org.name.lower()
-        app_name = app.name.lower()
+        org_name = org_path.name.lower()
+        app_name = app_path.name.lower()
         organizations[org_name] = {
             app_name: {
                 'installed': True,
                 'running': await get_app_running_status(app_name),
-                'instructions': await get_app_instructions(path.joinpath(org).joinpath(app).resolve()),
+                'instructions': await get_app_instructions(path.joinpath(org_path).joinpath(app_path).resolve()),
             }
         }
 
