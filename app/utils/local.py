6df5ce39b51f003e82273bc454d12e060005674a
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-16 19:22:18 -0300

    refactored moving get app path interactively to local lib

--- app/utils/local.py
+++ app/utils/local.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import pathlib
 from pathlib import Path
 
 import click
@@ -76,3 +77,35 @@ async def get_app_instructions(app_path: Path) -> dict:
     except Exception as exc:
         if global_options.VERBOSE:
             click.echo(f'Failed to load instructions from {app_path.name}: {exc}')
+
+
+async def get_app_path_interactively(app_name: str, org_name: str) -> pathlib.Path:
+    if org_name:
+        app_path = pathlib.Path(INSTALLATION_FOLDER, org_name, app_name).resolve()
+        if not app_path.exists():
+            raise click.UsageError(f'Install the app "{app_name}" first.')
+
+        return app_path
+
+    organizations = await get_organizations(INSTALLATION_FOLDER)
+    if not organizations:
+        raise click.exceptions.UsageError(f'Install the app "{app_name}" first.')
+
+    possible_orgs = [
+        org
+        for org in organizations
+        if org.joinpath(app_name).resolve().exists()
+    ]
+    if not possible_orgs:
+        raise click.exceptions.UsageError(f'Install the app "{app_name}" first.')
+
+    if len(possible_orgs) == 1:
+        return possible_orgs.pop().joinpath(app_name).resolve()
+
+    click.echo(f'The app "{app_name}" is present in more than one organization.')
+    click.echo(f'Which organization you want to use?')
+    for i, org in enumerate(organizations):
+        click.echo(f'{i + 1}) {org.name}')
+
+    choice = click.prompt('Choose organization number', type=int)
+    return possible_orgs[choice - 1].joinpath(app_name).resolve()
