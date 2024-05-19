68adf0e29f7775c4440c2f94c8dd209adf99c3e0
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-19 14:19:53 -0300

    refactored to turn num cols dynamic based on screen size

--- app/utils/printer.py
+++ app/utils/printer.py
@@ -1,5 +1,6 @@
 import asyncio
 import math
+import os
 import re
 
 import click
@@ -41,13 +42,14 @@ async def show_apps_by_organization(merged_apps_by_organization: dict):
 
 
 async def create_org_apps_table(merged_apps_by_organization: dict, org: str, organizations: [dict]):
-    formatted_matrix, max_width, num_cols = await format_matrix(merged_apps_by_organization[org])
+    formatted_matrix, max_cols_width, max_visible_width, num_cols = await format_matrix(
+        merged_apps_by_organization[org])
     table = texttable.Texttable()
     table.set_chars(['', '', '', ''])
-    table.set_cols_width([max_width] * num_cols)
+    table.set_cols_width([max_cols_width] * num_cols)
     table.add_rows(formatted_matrix, header=False)
     header_name = org.capitalize()
-    header_division_length = math.floor((max_width + num_cols / 2) * num_cols)
+    header_division_length = math.floor((max_visible_width + num_cols / 2) * num_cols)
     header_position = math.floor(header_division_length / 2 - len(header_name) / 2)
     organizations.append({
         'header': f'{" " * header_position}{header_name}',
@@ -56,26 +58,36 @@ async def create_org_apps_table(merged_apps_by_organization: dict, org: str, org
     })
 
 
-async def format_matrix(apps: dict) -> (list, int, int):
-    max_num_cols = 6
-    apps_with_status = await format_app_status(apps)
+async def format_matrix(apps: dict) -> (list, int, int, int):
+    word_pattern = re.compile(r'.*m(\w+).*')
+    apps_with_status = await format_app_status(apps, word_pattern)
+    app_names_width = []
+    app_names_visible_width = []
+
+    async with asyncio.TaskGroup() as tg:
+        for app_name in apps_with_status:
+            tg.create_task(get_app_name_width(app_name, word_pattern, app_names_width, app_names_visible_width))
+
+    max_col_width = max(app_names_width)
+    max_col_visible_width = max(app_names_visible_width)
+    size = os.get_terminal_size()
+    max_num_cols = size.columns // max_col_width
     app_quantity = len(apps_with_status)
     num_cols = app_quantity if app_quantity <= max_num_cols else max_num_cols
     num_rows = math.ceil(app_quantity / num_cols)
     formatted_apps_in_position = [['' for _ in range(num_cols)] for _ in range(num_rows)]
-    app_names_width = []
 
     async with asyncio.TaskGroup() as tg:
         for i, app_name in enumerate(apps_with_status):
-            tg.create_task(get_app_name_width(app_name, app_names_width))
             tg.create_task(set_app_position(app_name, formatted_apps_in_position, i, num_rows))
 
-    max_col_width = max(app_names_width)
-    return formatted_apps_in_position, max_col_width, num_cols
+    return formatted_apps_in_position, max_col_width, max_col_visible_width, num_cols
 
 
-async def get_app_name_width(app_name: str, app_names_width: [int]):
+async def get_app_name_width(app_name: str, word_pattern: re.Pattern, app_names_width: [int],
+                             app_names_visible_width: [int]):
     app_names_width.append(len(app_name))
+    app_names_visible_width.append(len(re.fullmatch(word_pattern, app_name).group(1)))
 
 
 async def set_app_position(app_name: str, formatted_apps_in_position: [[str]], i: int, num_rows: int):
@@ -84,14 +96,13 @@ async def set_app_position(app_name: str, formatted_apps_in_position: [[str]], i
     formatted_apps_in_position[line][column] = app_name
 
 
-async def format_app_status(apps: dict) -> list:
+async def format_app_status(apps: dict, word_patter: re.Pattern) -> list:
     apps_with_status = []
     async with asyncio.TaskGroup() as tg:
         for app in apps.keys():
             tg.create_task(add_icon_and_style(app, apps, apps_with_status))
 
-    word_pattern = re.compile(r'.*m(\w+).*')
-    apps_with_status.sort(key=lambda x: re.fullmatch(word_pattern, x).group(1))
+    apps_with_status.sort(key=lambda x: re.fullmatch(word_patter, x).group(1))
     return apps_with_status
 
 
