9cc54a87d02fa5a812e15a256851aacb824d6786
Alexandre Paes <alexandre.slp90@gmail.com>
2024-05-20 19:20:54 -0300

    added cd command

---
+++ app/commands/cd.py
@@ -0,0 +1,8 @@
+import subprocess
+
+from local import get_app_path_interactively
+
+
+async def cd_app_command(app_name: str, org_name: str):
+    app_path = await get_app_path_interactively(app_name, org_name)
+    subprocess.run(f'cd {app_path}', shell=True, check=True)
--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from app.commands.start import start_app_command
 from app.commands.stop import stop_app_command
 from app.commands.install import install_app_command
 from app.commands.remove import remove_app_command
+from app.commands.cd import cd_app_command
 from app.commands.version import show_app_version_command
 from utils import global_options
 
@@ -85,6 +86,20 @@ def remove(
 ):
     try:
         asyncio.run(remove_app_command(app_name, org_name))
+    except Exception as exc:
+        click.secho(f'Error: {exc}', fg='red', bold=True)
+        exit(1)
+
+
+@cli.command(help="Change to app's directory.")
+@click.argument('app_name')
+@click.option('-o', '--org', 'org_name', help='Organization name.')
+def cd(
+        app_name: str,
+        org_name: str,
+):
+    try:
+        asyncio.run(cd_app_command(app_name, org_name))
     except Exception as exc:
         click.secho(f'Error: {exc}', fg='red', bold=True)
         exit(1)
